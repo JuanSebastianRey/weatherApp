40f5217a6d173e0993efbf7a88bc6eaca0e40909
JuanSebastianRey <sebastianreyes25@outlook.es>
2024-10-21 07:08:18 -0500

    feat: :tada: Initial configuration

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+//react
+
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST, 
+      port: env.VITE_PORT, 
+    },
+  }
+});
